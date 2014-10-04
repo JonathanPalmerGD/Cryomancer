a0d154bb8af66289201b6b4f028ab6f39eda2f97
unknown <vantharion@gmail.com>
2014-10-03 22:16:58 -0400

    Added Ambient sounds system

---
+++ Assets/Editor/AmbientVolumeEditor.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(AmbientVolume))]
+public class AmbientVolumeEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		AmbientVolume myTarget = (AmbientVolume)target;
+
+		myTarget.player = (GameObject)EditorGUILayout.ObjectField(myTarget.player, typeof(GameObject));
+		myTarget.mesh = (Mesh)EditorGUILayout.ObjectField(myTarget.mesh, typeof(Mesh));
+
+		if (myTarget.ambAudios != null)
+		{
+			if(myTarget.ambAudios.Count > 0)
+			{
+				EditorGUILayout.LabelField("Number of Ambient Sounds: " + myTarget.ambAudios.Count);
+				EditorGUILayout.BeginVertical("Box");
+				DrawAmbients(myTarget);
+				EditorGUILayout.EndVertical();
+			}
+			if (GUILayout.Button("Add Ambient Volume"))
+			{
+				myTarget.ambAudios.Add(new AmbientAudio());
+			}
+		}
+		else
+		{
+			Debug.Log("Ambient Audio is null\n");
+			myTarget.ambAudios = new List<AmbientAudio>();
+		}
+		
+		//myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+		// EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+	}
+
+	public void DrawAmbients(AmbientVolume targ)
+	{
+		if (targ.ambAudios != null)
+		{
+			for (int i = 0; i < targ.ambAudios.Count; i++)
+			{
+				EditorGUILayout.BeginVertical("Box");
+
+				EditorGUILayout.LabelField(targ.ambAudios[i].name);
+				targ.ambAudios[i].name = EditorGUILayout.TextField("Name", targ.ambAudios[i].name);
+				targ.ambAudios[i].volume = EditorGUILayout.Slider("Volume", targ.ambAudios[i].volume, 0, 1);
+
+				DrawClips(i, targ);
+
+				DrawSliders(i, targ);
+
+				EditorGUILayout.EndVertical();
+			}
+		}
+		
+	}
+
+	private void DrawClips(int i, AmbientVolume targ)
+	{
+		EditorGUILayout.BeginVertical("Box");
+
+		if (targ.ambAudios[i].clips != null)
+		{
+			for (int j = 0; j < targ.ambAudios[i].clips.Count; j++)
+			{
+				targ.ambAudios[i].clips[j] = (AudioClip)EditorGUILayout.ObjectField(targ.ambAudios[i].clips[j], typeof(AudioClip));
+			}
+		}
+		else
+		{
+			targ.ambAudios[i].clips = new List<AudioClip>();
+		}
+		//Add new clip
+		if (GUILayout.Button("Add Clip"))
+		{
+			targ.ambAudios[i].clips.Add(new AudioClip());
+		}
+		EditorGUILayout.EndVertical();
+	}
+
+	private void DrawSliders(int i, AmbientVolume targ)
+	{
+		targ.ambAudios[i].minFreq = EditorGUILayout.Slider("Min Frequency", targ.ambAudios[i].minFreq, .5f, 30);
+
+		targ.ambAudios[i].playChance = EditorGUILayout.Slider("Play Chance", targ.ambAudios[i].playChance, 0f, 1f);
+
+		targ.ambAudios[i].sleepDuration = EditorGUILayout.Slider("Sleep Duration", targ.ambAudios[i].sleepDuration, -.2f, 30);
+
+		targ.ambAudios[i].sleepCounter = EditorGUILayout.Slider("Sleep Counter", targ.ambAudios[i].sleepCounter, -.2f, 30);
+
+		targ.ambAudios[i].sleeping = EditorGUILayout.Toggle("Currently Sleeping", targ.ambAudios[i].sleeping);
+	}
+}
---
+++ Assets/Scripts/AmbientVolume.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class AmbientVolume : MonoBehaviour
+{
+	[SerializeField]
+	public List<AmbientAudio> ambAudios;
+
+	public GameObject player;
+	public Mesh mesh;
+	private float counter = 0f;
+	private float threshold = 0f;
+
+	public void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		mesh = this.GetComponent<MeshFilter>().mesh;
+	}
+
+	public void Update()
+	{
+		counter += Time.deltaTime;
+		
+		if(collider.bounds.Contains(player.transform.position))
+		{
+			Debug.Log("Inside Mesh\n");
+			foreach (AmbientAudio aud in ambAudios)
+			{
+				if (aud.sleeping)
+				{
+					if (aud.sleepCounter > 0)
+					{
+						aud.sleepCounter -= Time.deltaTime;
+					}
+					if (aud.sleepCounter < 0)
+					{
+						aud.sleepCounter = 0;
+						aud.sleeping = false;
+					}
+				}
+			}
+
+			if (counter >= 3 + threshold)
+			{
+				counter -= 3.0f;
+				threshold += .2f;
+
+				RollForAmbient();
+			}
+
+		}
+		//while the player is inside
+		//Increase the counter of each ambient audio.
+		//If we exceed the counter, play that audio.
+	}
+
+	private void RollForAmbient()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			int playIndex = UnityEngine.Random.Range(0, ambAudios.Count);
+			Debug.Log("Rolling for ambient noise: " + playIndex + " out of " + ambAudios.Count + "\n");
+			if (ambAudios[playIndex].minFreq > counter && !ambAudios[playIndex].sleeping)
+			{
+				float randRange = UnityEngine.Random.Range(0.0f, 1.0f);
+				Debug.Log("Rolled: " + randRange + " out of " + ambAudios[playIndex].playChance + "\n");
+				if (randRange < ambAudios[playIndex].playChance)
+				{
+					PlayAudio(ambAudios[playIndex]);
+				}
+			}
+		}
+	}
+
+	private void PlayAudio(AmbientAudio ambientToPlay)
+	{
+		AudioSource audio = player.AddComponent<AudioSource>();
+		int randIndex = UnityEngine.Random.Range(0, ambientToPlay.clips.Count);
+		Debug.Log("Playing Audio: " + ambientToPlay.clips[randIndex].name);
+		audio.PlayOneShot(ambientToPlay.clips[randIndex]);
+		ambientToPlay.GotPlayed();
+	}
+}
+
+[Serializable]
+public class AmbientAudio
+{
+	public string name;
+	public float volume;
+	[SerializeField]
+	public List<AudioClip> clips;
+
+	public float playChance;
+
+	public float minFreq;
+
+	//If it is sleeping and for how long
+	public float sleepDuration;
+	public float sleepCounter;
+	public bool sleeping;
+
+	public AmbientAudio()
+	{
+		name = "New Ambient Sound";
+		volume = 1.0f;
+		minFreq = 10f;
+		sleepDuration = 10f;
+		sleepCounter = 3;
+		sleeping = true;
+		clips = new List<AudioClip>();
+
+	}
+
+	public void GotPlayed()
+	{
+		sleeping = true;
+		sleepCounter = sleepDuration;
+	}
+}
